ac23f9499ea3884694bfae8a7f437dcc2a14bcfb
Mikkelrydahl <90452638+Mikkelrydahl@users.noreply.github.com>
2022-03-09 15:19:28 +0100

    Add files via upload

---
+++ Obligatorisk Opgave 4/Obligatorisk Opgave 4/Controllers/CarController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Cors;
+using Obligatorisk_Opgave_4.Manager;
+
+namespace Obligatorisk_Opgave_4.Controllers
+{
+    
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CarsController : Controller
+    {
+
+        private CarManager _manager = new CarManager();
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [HttpGet]
+
+        public ActionResult<IEnumerable<Car>> Get([FromQuery] string modelFilter, [FromQuery] int? priceFilter,
+            [FromQuery] string licenseplateFilter)
+        {
+            IEnumerable<Car> car = _manager.GetAll(modelFilter, priceFilter, licenseplateFilter);
+
+            if (car.Count() <= 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(car);
+            }
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            Car car = _manager.GetById(id);
+            if (car == null) return NotFound("Not Found, id: " + id);
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public Car Post([FromBody] Car newCar)
+        {
+            return _manager.AddCars(newCar);
+        }
+
+        [HttpDelete("{id}")]
+        public Car Delete(int id)
+        {
+            Car car = _manager.GetById(id);
+            return _manager.Delete(id);
+        }
+
+
+    }
+    
+
+}
---
+++ Obligatorisk Opgave 4/Obligatorisk Opgave 4/Manager/CarManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_Opgave_4.Manager
+{
+    public class CarManager
+    {
+        private static int _nextId = 1;
+        private static readonly List<Car> data = new List<Car>
+
+        {
+            new Car {Id = _nextId++, Model = "BMVV", Price = 1000000, LicensePlate = "BMVV123"},
+            new Car {Id = _nextId++, Model = "Audi", Price = 2000000, LicensePlate = "Audi123"},
+            new Car {Id = _nextId++, Model = "Mercedes", Price = 3000000, LicensePlate = "Merce123"}
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List<Car> GetAll(string modelFilter, int? priceFilter, string? licenseplateFilter)
+        {
+            List<Car> result = new List<Car>(data);
+            if (!string.IsNullOrWhiteSpace(modelFilter))
+            {
+                result = result.FindAll(filterCar =>
+                    filterCar.Model.Contains(modelFilter, StringComparison.OrdinalIgnoreCase));
+            }
+
+            if (priceFilter != null)
+            {
+                result = result.FindAll(filterCar =>
+                    filterCar.Price <= priceFilter);
+            }
+
+            if (!string.IsNullOrWhiteSpace(licenseplateFilter))
+            {
+                result = result.FindAll(filterCar =>
+                    filterCar.LicensePlate.Contains(licenseplateFilter, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return result;
+        }
+
+        public Car GetById(int id)
+        {
+            return data.Find(car => car.Id == id);
+        }
+
+        public Car AddCars(Car newCar)
+        {
+            newCar.Id = _nextId;
+            data.Add(newCar);
+            return newCar;
+        }
+
+        public Car Delete(int id)
+        {
+            Car car = data.Find(car => car.Id == id);
+            if (car == null) return null;
+            data.Remove(car);
+            return car;
+        }
+    }
+}
